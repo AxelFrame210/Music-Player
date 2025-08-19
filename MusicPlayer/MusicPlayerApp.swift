0a10a8d4818d880aa4baf64a773e63c01f9e3e92
AxelFrame210 <duylekhanh.jeff1002@gmail.com>
2025-08-19 09:14:38 +0700

    Initial Commit

---
+++ MusicPlayer/MusicPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MusicPlayerApp.swift
+//  MusicPlayer
+//
+//  Created by Admin on 19/8/25.
+//
+
+import SwiftUI
+
+@main
+struct MusicPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
